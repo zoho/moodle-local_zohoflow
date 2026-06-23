2a6dbff3eb38e176c8b2413386f8739284baade8
Praveen R <52274306+praveen-r-6561@users.noreply.github.com>
2026-06-23 16:40:27 +0530

    Bug fixes

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
  defined('MOODLE_INTERNAL') || die();
 
- $plugin->version   = 2025120400;    // The current plugin version (Date: YYYYMMDDXX).
+ $plugin->version   = 2026062300;    // The current plugin version (Date: YYYYMMDDXX).
  $plugin->requires  = 2022041900;    // Requires this Moodle version.
  $plugin->component = 'local_zohoflow'; // Full name of the plugin (used for diagnostics).
- $plugin->release   = '1.0.0';
+ $plugin->release   = '1.1.0';
  $plugin->maturity  = MATURITY_STABLE; // Change to MATURITY_STABLE for production.
  $plugin->license   = 'GPLv3';
